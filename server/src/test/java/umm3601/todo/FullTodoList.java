1381c07ab7fdfa05cc83205e6574a1586bd6904c
Blake Johnson <joh18484@morris.umn.edu>
2021-02-15 22:59:39 -0600

    Created and Edited Final tests
    Created tests for filtering todos by a combination of filters as well as by limiting the list of todos. Also created a test for returning the full todo list.

--- server/src/test/java/umm3601/todo/FullTodoList.java
+++ server/src/test/java/umm3601/todo/FullTodoList.java
@@ -4,3 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.IOException;
 import java.util.HashMap;
 import org.junit.jupiter.api.Test;
+
+import jdk.jfr.Timestamp;
+
+public class FullTodoList {
+
+  @Test
+  public void totalTodoCount() throws IOException {
+    TodoDatabase db = new TodoDatabase("/todos.json");
+    Todo[] allTodos = db.listTodos(new HashMap<>());
+    assertEquals(300, allTodos.length);
+  }
+}
